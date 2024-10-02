9464689e38024ce4caf63e62faf20d8d44c8974a
shivambirla784 <86475649+shivambirla784@users.noreply.github.com>
2024-10-02 21:04:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
 //your JS code here. If required.
+// Book Class to represent a Book
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI Class to handle UI tasks
+class UI {
+    static displayBooks() {
+        const books = [];
+        books.forEach((book) => UI.addBookToList(book));
+    }
+
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><button class="delete">X</button></td>
+        `;
+
+        list.appendChild(row);
+    }
+
+    static deleteBook(el) {
+        if(el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+    static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
+}
+
+// Event: Add a Book
+document.querySelector('#submit').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    if(title === '' || author === '' || isbn === '') {
+        alert('Please fill in all fields');
+    } else {
+        const book = new Book(title, author, isbn);
+        UI.addBookToList(book);
+        UI.clearFields();
+    }
+});
+
+// Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    UI.deleteBook(e.target);
+});
